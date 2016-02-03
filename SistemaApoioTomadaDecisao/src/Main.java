a77b3526f5b9b728be583fb8375bbad14b185926
svaigen <alisson.svg@gmail.com>
2016-02-02 22:39:35 -0200

    .

---
+++ SistemaApoioTomadaDecisao/src/Main.java
@@ -0,0 +1,13 @@
+
+import javax.swing.JFrame;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        MenuPrincipal menuPrincipal = new MenuPrincipal();
+        menuPrincipal.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        menuPrincipal.setVisible(true);
+        
+    }
+}
